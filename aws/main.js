bc69cfb582d5e5c5f2184d6c4fd85bfcd4c7196a
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-04-17 17:34:20 +0200

    main.js

---
+++ aws/main.js
@@ -0,0 +1,69 @@
+/* Wetterstationen Euregio Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778,
+    zoom: 11,
+};
+
+// Karte initialisieren
+let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
+
+// thematische Layer
+let overlays = {
+    stations: L.featureGroup().addTo(map),
+}
+
+// Layer control
+L.control.layers({
+    "Relief avalanche.report": L.tileLayer(
+        "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+        attribution: `© <a href="https://sonny.4lima.de">Sonny</a>, <a href="https://www.eea.europa.eu/en/datahub/datahubitem-view/d08852bc-7b5f-4835-a776-08362e2fbf4b">EU-DEM</a>, <a href="https://lawinen.report/">avalanche.report</a>, all licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>`
+    }).addTo(map),
+    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+}, {
+    "Wetterstationen": overlays.stations,
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Wetterstationen
+async function loadStations(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+
+    // Wetterstationen mit Icons und Popups
+    //console.log(jsondata);
+    L.geoJSON(jsondata, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/wifi.png",
+                    iconSize: [32, 37],
+                    iconAnchor: [16, 37]
+                })
+            });
+        },
+        onEachFeature: function (feature, layer) {
+            //console.log(feature.properties);
+            console.log(feature.geometry);
+            let props = feature.properties;
+            layer.bindPopup(`
+                <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
+            `);
+        }
+    }).addTo(overlays.stations);
+}
+
+
+
+
+
+//GEOJSON laden
+loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
