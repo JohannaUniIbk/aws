cf70151a9a0c321b845a683247c89f7b785332a3
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-05-08 16:16:39 +0200

    Datum in local format hinzugefügt

--- main.js
+++ main.js
@@ -51,8 +51,10 @@ async function loadStations(url) {
             });
         },
         onEachFeature: function (feature, layer) {
+            let pointInTime = new Date(feature.properties.date);
+            console.log(pointInTime);
             //console.log(feature.properties);
-            console.log(feature.geometry);
+            //console.log(feature.geometry);
             let props = feature.properties;
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
@@ -62,7 +64,7 @@ async function loadStations(url) {
                     <li>Windgeschwindigkeit (km/h) ${feature.properties.WG ||"-"}</li>
                     <li>Schneehöhe (cm) ${feature.properties.HS ||"-"}</li>
                 </ul>
-                <span></span>
+                <span>${pointInTime.toLocaleString()}</span>
             `);
         }
     }).addTo(overlays.stations);
